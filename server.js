669cea743d06295ad1c51612e73c58c944b59b6a
deepyes02 <deepesh.dhakal@digitalwallet.co.jp>
2024-10-22 13:34:05 +0900

    Ai chatbot

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const app = express();
 
-const PORT = 3000;
+const PORT = 80;
 const aiEndpoint = "http://127.0.0.1:11434/api/generate";
 
 app.use(express.json());
@@ -37,36 +37,26 @@ app.post('/ask-ai', async (req, res) => {
 // Function to make the POST request to the AI endpoint
 async function makePostRequestToAI(userRequest, clientRes) {
 	const { model, prompt } = userRequest;
-
 	const requestData = {
 		model: model,
 		prompt: prompt,
 	};
-
 	const aiResponse = await fetch(aiEndpoint, {
 		method: 'POST',
 		body: JSON.stringify(requestData)
 	})
-
 	if (!aiResponse.ok) { throw new Error('Failed to get response from AI') }
-
 	const reader = aiResponse.body.getReader();
 	const decoder = new TextDecoder('utf-8');
-
 	clientRes.setHeader('Content-Type', 'application/x-ndjson');
-
-
 	while (true) {
 		const { done, value } = await reader.read();
 		if (done) break;
-
 		const chunk = decoder.decode(value, { stream: true })
 		clientRes.write(chunk)
 	}
-
 	clientRes.end();
 }
-
 // Start the server
 app.listen(PORT, () => {
 	console.log(`AI Server running at http://localhost:${PORT}`);
